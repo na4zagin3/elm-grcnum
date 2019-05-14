0d0321a3c147f6bd484d9d8a9046e95833cb1fa8
na4zagin3 <mrty.ityt.pt@gmail.com>
2019-05-14 19:19:47 +0900

    Refine SexagesimalTripleSpec

--- tests/SexagesimalTripleSpec.elm
+++ tests/SexagesimalTripleSpec.elm
@@ -6,26 +6,27 @@ import Expect exposing (Expectation, pass, fail)
 import Fuzz exposing (Fuzzer, int, list, string, tuple, tuple3)
 import Test exposing (..)
 
+zero = BigInt.fromInt 0
+
+bigint =
+    let base = BigInt.fromInt 3600 in
+    let int3600 =
+            Fuzz.intRange 0 3600 in
+    let convn (a, b, c) =
+            BigInt.fromInt a
+                |> BigInt.mul base
+                |> BigInt.add (BigInt.fromInt b)
+                |> BigInt.mul base
+                |> BigInt.add (BigInt.fromInt c) in
+    tuple3 (int3600, int3600, int3600) |> Fuzz.map convn
 
 suite : Test
 suite =
     describe "SexagesimalTriple"
         [ describe "SexagesimalTriple.secondsToCommon"
-              [let sg = tuple3 (int, int, int) in
-               fuzz2 sg sg "return too big for all numbers which is bigger than a number" <|
-                    \u v ->
+              [fuzz2 bigint bigint "return too big for all numbers which is bigger than a number" <|
+                    \n m ->
                     let conv x = x |> SexagesimalTriple.secondsToCommon in
-                    let zero = BigInt.fromInt 0 in
-                    let base = BigInt.fromInt 3600 in
-                    -- ToDo: Add a Fuzzer for BigInt
-                    let convn (a, b, c) =
-                          BigInt.fromInt a
-                          |> BigInt.mul base
-                          |> BigInt.add (BigInt.fromInt b)
-                          |> BigInt.mul base
-                          |> BigInt.add (BigInt.fromInt c) in
-                    let n = convn u in
-                    let m = convn v in
                     let a = BigInt.min n m in
                     let b = BigInt.max n m in
                     case (BigInt.compare a zero, conv a, conv b) of
